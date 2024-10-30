68741f675cf156e90561bdb75ac51309905baa91
Eklund2012 <david.eklund9@gmail.com>
2024-10-30 20:46:32 +0100

    general coding changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,43 @@
-    const toggleButton = document.querySelector('.theme-switch input[type="checkbox"]');
+/*
+TODO make light mode look good
+const toggleButton = document.querySelector('.theme-switch input[type="checkbox"]');
+const socialIcons = document.querySelectorAll('.social-icon');
 
-    // Load the theme from local storage if available
-    const currentTheme = localStorage.getItem('theme');
-    if (currentTheme) {
-        document.body.classList.toggle('light', currentTheme === 'light');
-    }
+// Load the theme from local storage if available
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.body.classList.toggle('light', currentTheme === 'light');
+    updateIconSources(currentTheme);
+}
 
-    toggleButton.addEventListener('click', () => {
-        // Toggle the class on the body
-        document.body.classList.toggle('light');
+toggleButton.addEventListener('click', () => {
+    // Toggle the theme on the body
+    document.body.classList.toggle('light');
+    
+    // Get the new theme
+    const theme = document.body.classList.contains('light') ? 'light' : 'dark';
+    
+    // Save the theme to local storage
+    localStorage.setItem('theme', theme);
+    
+    // Update icon sources based on the theme
+    updateIconSources(theme);
+});
 
-        // Save the current theme in local storage
-        const theme = document.body.classList.contains('light') ? 'light' : 'dark';
-        localStorage.setItem('theme', theme);
-        
-        // Update button text
-        toggleButton.textContent = document.body.classList.contains('light') ? '🌙' : '☀️';
+// Function to update icon sources based on the theme
+function updateIconSources(theme) {
+    socialIcons.forEach(icon => {
+        icon.src = theme === 'light' ? icon.dataset.lightSrc : icon.dataset.darkSrc;
     });
+}*/
+
+function toggleSection(contentId) {
+    const content = document.getElementById(contentId);
+
+    // Toggle the 'open' class only on mobile
+    if (window.innerWidth <= 768) {
+        content.classList.toggle('open');
+    }
+}
+
+
